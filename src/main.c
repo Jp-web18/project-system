a053fcf2b33c30963fd1b1e522e0977c5ca981fb
jp-web-space <2401101011@student.buksu.edu.ph>
2025-01-22 14:56:27 +0800

    sample_codes

--- src/main.c
+++ src/main.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main() {
-	char prompts[][50] = {
-		"Enter a number: ",
-		"Enter another number: ",
-		"Enter again a number: "
-	};
-	char inputVal[10];
-	int num[3];
-	
-	printf("Enter a number: ");
-	for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++) {
-		while (true) {
-			if (fgets(inputVal, sizeof(inputVal), stdin)) {
-				if (inputVal[0] == '\n') {
-					printf("Empty input. Enter a number: ");
-					continue;
-				} else if (sscanf(inputVal, "%d", &num[i])) {
-					break;
-				} else {
-					printf("Invalid input. Enter a number: ");
-				}
-			}
-		}
-		/* code */
-	}
-	
-	for (size_t i = 0; i < sizeof(num) / sizeof(num[0]); i++) {
-		printf("You entered: %d\n", num[i]);
-		/* code */
-	}
-	
-	printf("You entered: %d\n", num);
-	
+  const char *promptsForInfo[] = {
+    "Enter your favorite number: ",
+    "Enter age: ",
+    "Enter years of experience: ",
+    "Enter grade: ",
+    "Enter number of siblings: "
+  };
+  float info[5];
+  char inputVal[10];
 
-	return 0;
+  printf("Enter basic informations:\n");
+  for (int i = 0; i < 5; i++) {
+    printf("%s", promptsForInfo[i]);
+    // scanf("%f", &grades[i]);
+    while (true) {
+      if (fgets(inputVal, sizeof(inputVal), stdin)) {
+        if (inputVal[0] == '\n') {
+          printf("\nInvalid empty input! Enter again.\n");
+          printf("%s", promptsForInfo[i]);
+          continue;
+          // printf("%s", subjects[i]);
+        } else if (sscanf(inputVal, "%f", &info[i])) {
+          break;
+        }
+      }
+
+      printf("\nInvalid info entry! Enter another valid info.\n");
+      printf("%s", promptsForInfo[i]);
+    }
+  }
+
+  char *infoNames[] = {
+		"\nFavorite number",
+		"Age",
+		"Years of experience",
+		"Grade",
+		"Number of siblings"
+	};
+
+	for (int i = 0; i < 5; i++) {
+		printf("%s: %.2f\n", infoNames[i], info[i]);
+	}
+
+  return 0;
 }
